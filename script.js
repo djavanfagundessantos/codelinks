515e69e3a1f7ca84b807bb7e009496d4a894c197
Djavan Fagundes dos Santos <djavan.fagundes.santos@gmail.com>
2024-10-27 15:02:12 -0300

    add new foto and img properties

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/oculos.jpg")
     img.setAttribute(
       "alt",
       "Foto de uma pessoa de óculos escuros, com roupa preta e fundo gradiente do roxo para o azul"
     )
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/sem-oculos.jpg")
     img.setAttribute(
       "alt",
       "Foto de uma pessoa de óculos, com roupa preta e fundo gradiente do roxo para o azul"
